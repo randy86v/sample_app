f99a116ce85217f99ff5a05fd4b22c40fad69489
saasbook <saasbook@saasbook.(none)>
2012-10-24 06:56:44 -0700

    Finish Exercise of Chapter 5

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "User pages" do
 
+  #tells RSpec that page is the subject of the tests.
+  #the call to should command(method) automatically uses the page variable supplied by capybara.
   subject { page }
   
   describe "signup page" do
@@ -9,5 +11,6 @@ describe "User pages" do
     
     it { should have_selector('h1', text: 'Sign up') }
     it { should have_selector('title', text: full_title('Sign up')) }
+    #full_title is a test helper method defined in spec/support/utilities.rb
   end
 end
